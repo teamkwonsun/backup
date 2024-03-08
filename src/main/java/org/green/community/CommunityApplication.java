7603783ba2dbd1c9acac0ff833f8cb69928f9a5f
teamkwonsun <forstudy12323@gamail.com>
2024-03-08 09:57:24 +0900

    com to note 3.8

--- src/main/java/org/green/community/CommunityApplication.java
+++ src/main/java/org/green/community/CommunityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing //http://localhost:8084/board/list
 public class CommunityApplication {
 
 	public static void main(String[] args) {
